d56e73c44bb3ae0b0f9a2992649313be8cdcd270
Matheus Carnaúba de Souza <77417438+MCARNAUBA@users.noreply.github.com>
2022-04-06 19:59:26 -0300

    Subindo arquivo do projeto

---
+++ conexao.php
@@ -0,0 +1,5 @@
+<?php
+
+//host, usuário, senha, nome do banco
+
+$conn = mysqli_connect('us-cdbr-east-05.cleardb.net', 'be255b6ec2a1eb', '24f00a8e', 'heroku_8c92769b4870240') or die('Erro ao conectar ao banco de dados');
