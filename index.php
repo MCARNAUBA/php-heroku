34dbd2387d7f0997edd376fde746a68b61dcc48b
Matheus Carnaúba de Souza <77417438+MCARNAUBA@users.noreply.github.com>
2022-04-06 21:02:34 -0300

    teste

--- index.php
+++ index.php
@@ -67,7 +67,7 @@
 					<div class="row row g-2">
   						<div class="col col-sm">
 							<label class="form-label">Código: </label
-							<input type="number" id="codigo" name="" class="form-control" value="">
+							<input type="number" id="codigo" name="" class="form-control" value=""></input>
 							<small class="form-text text-muted"><em>Somente números.</em></small>
 						</div>
 						<div class="col col-sm-10">
@@ -80,7 +80,7 @@
 					<div class="row  row g-2">
   						<div class="col col-sm">
 							<label class="form-label">CPF: </label>
-							<input type="text" id="cpf" name="" class="form-control" value="9999999">
+							<input type="text" id="cpf" name="" class="form-control" value="">
 							<small  class="form-text text-muted"><em>Somente números.</em></small>
 						</div>
 						<div class="col col-sm-8">
