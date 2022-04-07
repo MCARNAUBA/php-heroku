b9cf44b6dc5a4509d3f60b98f94baf46d099e496
Matheus Carnaúba de Souza <77417438+MCARNAUBA@users.noreply.github.com>
2022-04-06 21:15:02 -0300

    formatação

--- index.php
+++ index.php
@@ -107,21 +107,19 @@
 								}
 							</script>
 					</form>
-						</div>
 
 				</div>
 
 			</div>
 
-
-
 			<div style="height: 100px;"></div>
 
 
-			<div class="footer-copyright fixed-bottom bg-primary text-center py-3">
-				<span class="text-light align-middle">| ::: NetChute - Artigos esportivos. ::: UC 14 | - ©
-					2022 Copyright: CTM Supervisão de Educação Online</span>
-						</div>
+				<div class="footer-copyright fixed-bottom bg-primary text-center py-3">
+					<span class="text-light align-middle">| ::: NetChute - Artigos esportivos. ::: UC 14 | - ©
+						2022 Copyright: CTM Supervisão de Educação Online</span>
+				</div>
+			</div>
 
 		</div>
 	</div>
